84eb16dc4fcda9424ae4e24f4fc9b3d351b80627
ncocana <ncocana@cifpfbmoll.eu>
2023-04-09 20:53:57 +0200

    feat(app, jpa_service, database, persistence.xml): add mock data

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/App.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/App.java
@@ -1,13 +1,43 @@
 package edu.craptocraft.mariadb_jpa_jdbc;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+import java.util.List;
+
+import edu.craptocraft.mariadb_jpa_jdbc.jpa_service.JPAService;
+import edu.craptocraft.mariadb_jpa_jdbc.database.programming_language.ProgrammingLanguage;
+ 
+public class App {
+ 
+    private static final JPAService jpaService = JPAService.getInstance();
+ 
+    public static void main(String[] args) {
+        try {
+            createProgrammingLanguages();
+            printProgrammingLanguages();
+        } finally {
+            jpaService.shutdown();
+        }
     }
+
+    private static void createProgrammingLanguages() {
+        jpaService.runInTransaction(entityManager -> {
+            Arrays.stream("Java,C++,C#,JavaScript,Rust,Go,Python,PHP".split(","))
+                    .map(name -> new ProgrammingLanguage(name, (int) (Math.random() * 10)))
+                    .forEach(entityManager::persist);
+            return null;
+        });
+    }
+
+    private static void printProgrammingLanguages() {
+        List<ProgrammingLanguage> programmingLanguages = jpaService.runInTransaction(entityManager ->
+                entityManager.createQuery(
+                        "select p from ProgrammingLanguage p",
+                        ProgrammingLanguage.class
+                ).getResultList());
+ 
+        programmingLanguages.stream()
+                .map(pl -> pl.getName() + ": " + pl.getRating())
+                .forEach(System.out::println);
+    }
+
 }

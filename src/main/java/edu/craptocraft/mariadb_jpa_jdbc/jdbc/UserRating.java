059e522331ed304f35ca510f1dd84a00a537a58c
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-04-11 22:23:34 +0200

    perf(Strings): Eliminado bloques de textos

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/jdbc/UserRating.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/jdbc/UserRating.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 
 public class UserRating {
 
-    public static HikariDataSource dataSource;
+    private static HikariDataSource dataSource;
 
     public UserRating() {
     };
@@ -20,9 +20,8 @@ public class UserRating {
         System.out.println("Creating data...");
 
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("""
-                    INSERT INTO user_rating(rating, string_rating) VALUES(?, ?)
-                    """)) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("INSERT INTO user_rating(rating, string_rating) VALUES(?, ?)")) {
                 statement.setInt(1, value);
                 statement.setString(2, stringValue);
                 int rowsInserted = statement.executeUpdate();
@@ -38,9 +37,7 @@ public class UserRating {
         dataSource = Database.getConnectionPool();
 
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("""
-                    SELECT * FROM user_rating
-                    """)) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM user_rating")) {
                 ResultSet resultSet = statement.executeQuery();
                 boolean empty = true;
                 while (resultSet.next()) {
@@ -52,11 +49,28 @@ public class UserRating {
                 }
 
                 if (empty) {
-                    System.out.println("\t No data on the database");
+                    System.out.println("\t No data in the database");
                 }
             }
         }
 
+    }
+
+    public static void updateData(int rating, String newStringRating) throws SQLException {
+
+        dataSource = Database.getConnectionPool();
+
+        System.out.println("Updating data...");
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("UPDATE user_rating SET string_rating = ? WHERE rating LIKE ?")) {
+                statement.setString(1, newStringRating);
+                statement.setInt(2, rating);
+                int rowsUpdated = statement.executeUpdate();
+                System.out.println("Rows updated: " + rowsUpdated);
+
+            }
+        }
     }
 
     public static void deleteData(String nameExpression) throws SQLException {
@@ -66,9 +80,8 @@ public class UserRating {
         dataSource = Database.getConnectionPool();
 
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("""
-                        DELETE FROM user_rating WHERE string_rating LIKE ?
-                    """)) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("DELETE FROM user_rating WHERE string_rating LIKE ?")) {
                 statement.setString(1, nameExpression);
                 int rowsDeleted = statement.executeUpdate();
                 System.out.println("Rows deleted: " + rowsDeleted);

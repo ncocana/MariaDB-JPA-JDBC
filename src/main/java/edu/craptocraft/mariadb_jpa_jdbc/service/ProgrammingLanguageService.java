cbe70aa16e3409593efb724809175192a0afaeb2
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-04-11 21:31:39 +0200

    feat(): Agregado nuevo generador random para dev_rating

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/service/ProgrammingLanguageService.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/service/ProgrammingLanguageService.java
@@ -9,27 +9,32 @@ public class ProgrammingLanguageService {
 
     private static final JPAService jpaService = JPAService.getInstance();
 
-    private ProgrammingLanguageService() {}
+    private ProgrammingLanguageService() {
+    }
 
     public static void createData() {
         jpaService.runInTransaction(entityManager -> {
             Arrays.stream("Java,C++,C#,JavaScript,Rust,Go,Python,PHP".split(","))
-                    .map(name -> new ProgrammingLanguage(name, (int) (Math.random() * 10)))
+                    .map(name -> new ProgrammingLanguage(name, (int) (Math.random() * 10),
+                            (int) ((Math.random() * 10))))
                     .forEach(entityManager::persist);
             return null;
         });
     }
 
     public static void readData() {
-        List<ProgrammingLanguage> programmingLanguages = jpaService.runInTransaction(entityManager ->
-                entityManager.createQuery(
+        StringBuilder programmingData = new StringBuilder();
+        List<ProgrammingLanguage> programmingLanguages = jpaService
+                .runInTransaction(entityManager -> entityManager.createQuery(
                         "select p from ProgrammingLanguage p",
-                        ProgrammingLanguage.class
-                ).getResultList());
- 
+                        ProgrammingLanguage.class).getResultList());
+
         programmingLanguages.stream()
-                .map(pl -> pl.getName() + ": " + pl.getRating())
-                .forEach(System.out::println);
+                .map(pl -> "\nProgramming Language name: " + pl.getName() + "\nUser Rating: " + pl.getUserRating()
+                        + "\nDeveloper Rating: " + pl.getDevRating() + "\n")
+                .forEach(programmingData::append);
+
+        System.out.print(programmingData.toString());
     }
 
     public static void shutdownDatabase() {

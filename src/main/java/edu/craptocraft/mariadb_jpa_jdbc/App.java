8c7f388e2bcfc6118088d4f9c9baa97382a95c16
ncocana <ncocana@cifpfbmoll.eu>
2023-04-13 00:52:06 +0200

    feat(App): improve output application and crud

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/App.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/App.java
@@ -1,68 +1,34 @@
 package edu.craptocraft.mariadb_jpa_jdbc;
 
-import java.sql.SQLException;
-
-import edu.craptocraft.mariadb_jpa_jdbc.jdbc.Database;
+import edu.craptocraft.mariadb_jpa_jdbc.service.DeveloperRatingService;
 import edu.craptocraft.mariadb_jpa_jdbc.service.ProgrammingLanguageService;
-import edu.craptocraft.mariadb_jpa_jdbc.jdbc.UserRating;
-import edu.craptocraft.mariadb_jpa_jdbc.jdbc.DeveloperRating;
+import edu.craptocraft.mariadb_jpa_jdbc.service.UserRatingService;
 
 public class App {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         try {
-            System.out.println("JPA Services");
-            ProgrammingLanguageService.createData();
-            ProgrammingLanguageService.readData();
+            System.out.println("JPA SERVICE");
+
+            UserRatingService.createData();
+            System.out.println("\nUser rating:\n");
+            System.out.println(UserRatingService.readData());
+            
+            DeveloperRatingService.createData();
+            System.out.println("\nDeveloper rating:\n");
+            System.out.println(DeveloperRatingService.readData());
+
+            ProgrammingLanguageService.createData("Java", UserRatingService.getDataRating(8), DeveloperRatingService.getDataRating(9));
+            ProgrammingLanguageService.createData("C++", UserRatingService.getDataRating(7), DeveloperRatingService.getDataRating(8));
+            ProgrammingLanguageService.createData("C#", UserRatingService.getDataRating(6), DeveloperRatingService.getDataRating(7));
+            ProgrammingLanguageService.createData("JavaScript", UserRatingService.getDataRating(9), DeveloperRatingService.getDataRating(9));
+            System.out.println("Programming Languages:");
+            System.out.println(ProgrammingLanguageService.readData());
         } finally {
+            UserRatingService.shutdownDatabase();
+            DeveloperRatingService.shutdownDatabase();
             ProgrammingLanguageService.shutdownDatabase();
-        }
-
-        try {
-            System.out.println("Pools Connections");
-            Database.initDatabaseConnectionPool();
-            UserRating.deleteData("%");
-            for (int i = 0; i <= 10; i++) {
-                if (i >= 0 && i <= 4) {
-                    UserRating.createData(i, "Bad language");
-                } else if (i >= 5 && i <= 6) {
-                    UserRating.createData(i, "Regular language");
-                } else if (i >= 7 && i <= 8) {
-                    UserRating.createData(i, "Good language");
-                } else {
-                    UserRating.createData(i, "Excellent language");
-                }
-            }
-            UserRating.readData();
-            UserRating.updateData(10, "Perfect language for learning");
-            UserRating.readData();
-
-        } finally {
-            Database.closeDatabaseConnectionPool();
-        }
-
-        try {
-            System.out.println("Plains connections");
-            Database.openDatabaseConnection();
-            DeveloperRating.deleteData("%");
-            for (int i = 0; i <= 10; i++) {
-                if (i >= 0 && i <= 4) {
-                    DeveloperRating.createData(i, "Bad language for working");
-                } else if (i >= 5 && i <= 6) {
-                    DeveloperRating.createData(i, "Regular language for working");
-                } else if (i >= 7 && i <= 8) {
-                    DeveloperRating.createData(i, "Good language for working");
-                } else {
-                    DeveloperRating.createData(i, "Perfect language for developers");
-                }
-            }
-            DeveloperRating.readData();
-            DeveloperRating.updateData(10, "The best language for developers");
-            DeveloperRating.readData();
-
-        } finally {
-            Database.closeDatabaseConnection();
         }
 
     }

61096955b0aaa6dd8fbb62703fc01350fa67fbc8
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-04-16 01:52:13 +0200

    feat():Añadido autowired

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/service/ProgrammingLanguageService.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/service/ProgrammingLanguageService.java
@@ -2,12 +2,15 @@ package edu.craptocraft.mariadb_jpa_jdbc.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import edu.craptocraft.mariadb_jpa_jdbc.entity.ProgrammingLanguage;
 import edu.craptocraft.mariadb_jpa_jdbc.entity.DeveloperRating;
 import edu.craptocraft.mariadb_jpa_jdbc.entity.UserRating;
 
 public class ProgrammingLanguageService {
 
+    @Autowired
     private static final JPAService jpaService = JPAService.getInstance();
 
     private ProgrammingLanguageService() {

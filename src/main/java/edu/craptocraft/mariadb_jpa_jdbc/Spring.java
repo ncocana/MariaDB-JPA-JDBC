fcf764059da4c8caca13d5124af1f1aee4e1c59b
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-04-16 01:53:19 +0200

    feat(main): Añadido clase main para ejecutar la app

---
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/Spring.java
@@ -0,0 +1,13 @@
+package edu.craptocraft.mariadb_jpa_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring {
+    public static void main(String[] args) {
+        SpringApplication.run(Spring.class, args);
+
+    }
+
+}

54d3781122bfd71ba8b911d3344427a7a596449d
ncocana <ncocana@cifpfbmoll.eu>
2023-04-16 23:58:57 +0200

    refactor(ProgrammingLanguageController): improve endpoints

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/Controller/ProgrammingLanguageController.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/Controller/ProgrammingLanguageController.java
@@ -1,4 +1,4 @@
-package edu.craptocraft.mariadb_jpa_jdbc.Controller;
+package edu.craptocraft.mariadb_jpa_jdbc.controller;
 
 import java.util.List;
 
@@ -8,47 +8,50 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import edu.craptocraft.mariadb_jpa_jdbc.entity.ProgrammingLanguage;
-
-import edu.craptocraft.mariadb_jpa_jdbc.service.Implementation.PLIMPL;
+import edu.craptocraft.mariadb_jpa_jdbc.service.springboot.implementation.ProgLangIMPL;
 
 @RestController
-@RequestMapping("/programmingLanguages")
+@RequestMapping("/programming-languages")
 public class ProgrammingLanguageController {
 
     @Autowired
-    private PLIMPL impl;
+    private ProgLangIMPL impl;
 
     @PostMapping
-    @RequestMapping(value = "createLanguage", method = RequestMethod.POST)
-    public ResponseEntity<?> createLanguage(@RequestBody ProgrammingLanguage language) {
+    @RequestMapping(value = "create/{id}", method = RequestMethod.POST)
+    public ResponseEntity<?> createLanguage(@PathVariable int id, @RequestBody ProgrammingLanguage language) {
+        ProgrammingLanguage existingLanguage = this.impl.readData(id);
+        if (existingLanguage != null) {
+            return ResponseEntity.badRequest().body("Invalid ID: a ProgrammingLanguage object with ID " + id + " already exists.");
+        }
         ProgrammingLanguage createdLanguage = this.impl.createData(language);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdLanguage);
     }
 
     @GetMapping
-    @RequestMapping(value = "readLanguages", method = RequestMethod.GET)
-    public ResponseEntity<?> readLanguage() {
+    @RequestMapping(value = "get/all", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllLanguages() {
         List<ProgrammingLanguage> listLanguage = this.impl.readData();
         return ResponseEntity.ok(listLanguage);
     }
 
     @GetMapping
-    @RequestMapping(value = "readLanguage/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> readLanguage(@PathVariable int id) {
+    @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> readOneLanguage(@PathVariable int id) {
         ProgrammingLanguage singleLanguage = this.impl.readData(id);
         return ResponseEntity.ok(singleLanguage);
     }
 
     @PutMapping
-    @RequestMapping(value = "updateLanguage", method = RequestMethod.PUT)
-    public ResponseEntity<?> updateLanguage(@RequestBody ProgrammingLanguage language) {
-        ProgrammingLanguage updatedLanguage = this.impl.updateData(language);
+    @RequestMapping(value = "update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateLanguage(@PathVariable int id, @RequestBody ProgrammingLanguage language) {
+        ProgrammingLanguage updatedLanguage = this.impl.updateData(id, language);
         return ResponseEntity.status(HttpStatus.CREATED).body(updatedLanguage);
     }
 
     @DeleteMapping
-    @RequestMapping(value = "deleteLanguage/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<?> deletedLanguage(@PathVariable int id) {
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteLanguage(@PathVariable int id) {
         this.impl.deleteData(id);
         return ResponseEntity.ok().build();
     }

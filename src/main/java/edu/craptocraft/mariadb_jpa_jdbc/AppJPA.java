08ed575fb5e98c33ba393b831d80fddd0993d216
ncocana <ncocana@cifpfbmoll.eu>
2023-04-21 01:24:14 +0200

    fix(AppJPA, persistence, README): fix database's data persistance in docker volume

--- src/main/java/edu/craptocraft/mariadb_jpa_jdbc/AppJPA.java
+++ src/main/java/edu/craptocraft/mariadb_jpa_jdbc/AppJPA.java
@@ -9,35 +9,40 @@ public class AppJPA {
     public static void main(String[] args) {
 
         try {
-            System.out.println("JPA SERVICE");
+            // If the entity does not exist, create it and insert some mock data.
+            try {
+                System.out.println("JPA SERVICE");
 
-            // Creates the table "user_rating" and inserts data in it.
-            UserRatingService.createData();
-            System.out.println("\nUser rating:\n");
-            System.out.println(UserRatingService.readData());
+                // Creates the table "user_rating" and inserts data in it.
+                UserRatingService.createData();
+                System.out.println("\nUser rating:\n");
+                System.out.println(UserRatingService.readData());
 
-            // Creates the table "dev_rating" and inserts data in it.
-            DeveloperRatingService.createData();
-            System.out.println("\nDeveloper rating:\n");
-            System.out.println(DeveloperRatingService.readData());
+                // Creates the table "dev_rating" and inserts data in it.
+                DeveloperRatingService.createData();
+                System.out.println("\nDeveloper rating:\n");
+                System.out.println(DeveloperRatingService.readData());
 
-            // Creates the table "programming_language" and inserts data in it.
-            ProgrammingLanguageServiceJPA.createData("Java", 8, 9);
-            ProgrammingLanguageServiceJPA.createData("C++", 7, 8);
-            ProgrammingLanguageServiceJPA.createData("C#", 6, 7);
-            ProgrammingLanguageServiceJPA.createData("JavaScript", 9, 9);
+                // Creates the table "programming_language" and inserts data in it.
+                ProgrammingLanguageServiceJPA.createData("Java", 8, 9);
+                ProgrammingLanguageServiceJPA.createData("C++", 7, 8);
+                ProgrammingLanguageServiceJPA.createData("C#", 6, 7);
+                ProgrammingLanguageServiceJPA.createData("JavaScript", 9, 9);
 
-            // Shows the data in the table "programming_language".
-            System.out.println("Programming Languages:");
-            System.out.println(ProgrammingLanguageServiceJPA.readData());
+                // Shows the data in the table "programming_language".
+                System.out.println("Programming Languages:");
+                System.out.println(ProgrammingLanguageServiceJPA.readData());
 
-            // Updates the data in the table "programming_language" and shows the updated data.
-            ProgrammingLanguageServiceJPA.updateData(3, "XML", 9, 8);
-            System.out.println(ProgrammingLanguageServiceJPA.readData());
+                // Updates the data in the table "programming_language" and shows the updated data.
+                ProgrammingLanguageServiceJPA.updateData(3, "XML", 9, 8);
+                System.out.println(ProgrammingLanguageServiceJPA.readData());
 
-            // Deletes the data in the table "programming_language" and shows the updated data.
-            ProgrammingLanguageServiceJPA.deleteData(3);
-            System.out.println(ProgrammingLanguageServiceJPA.readData());
+                // Deletes the data in the table "programming_language" and shows the updated data.
+                ProgrammingLanguageServiceJPA.deleteData(3);
+                System.out.println(ProgrammingLanguageServiceJPA.readData());
+            } catch (Exception e) {
+                // If the entity already exists, do nothing.
+            }
         } finally {
             // Closes the instances of each service connecting to the database.
             UserRatingService.shutdownDatabase();
